5bd9fbf02be03cfa0780cad1a1f980cbf25c2b1a
Sayan Goswami <goswami.sayan47@gmail.com>
2021-04-20 22:27:03 +0200

    Implement parsing of config file.

--- lab01/clientserver.go
+++ lab01/clientserver.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"bufio"
+	"flag"
+	"io/ioutil"
+	"log"
 	_ "net"
+	"strings"
 )
 
 func main() {
+	// Setup and parse CLI flags.
+	configFile := flag.String("config", "this is not a path", "Path to config file.")
+	flag.Parse()
 
+	if *configFile == "this is not a path" {
+		panic("Invalid config file, please pass a valid config file.")
+	} else {
+		log.Println("Reading configuration from: " + *configFile)
+	}
+
+	// Read data from config file.
+	configBytes, err := ioutil.ReadFile(*configFile)
+	if err != nil {
+		panic("Error reading config file.")
+	}
+
+	// Parse config file.
+	configData := string(configBytes)
+	scanner := bufio.NewScanner(strings.NewReader(configData))
+
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), ":")
+		log.Println(line)
+	}
 }
