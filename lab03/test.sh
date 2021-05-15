900611cf588dd12f61af0598cdbacef2ca87af31
Sayan Goswami <goswami.sayan47@gmail.com>
2021-05-15 21:31:29 +0200

    Update config files.

--- lab03/test.sh
+++ lab03/test.sh
@@ -7,13 +7,7 @@ set numInstances to 5
 
 tell application "iTerm2"
     tell current window
-        set configFiles to {}
-
-        repeat with n from 1 to numInstances
-            set cfgfile to "config/configFile_600" & n & ".txt"
-            set configFiles to configFiles & {cfgfile}
-        end repeat
-
+        set configFiles to {"configFile_01.txt", "configFile_23.txt", "configFile_43.txt", "configFile_55.txt", "configFile_02.txt", "configFile_24.txt", "configFile_44.txt", "configFile_56.txt", "configFile_11.txt", "configFile_31.txt", "configFile_45.txt", "configFile_57.txt", "configFile_12.txt", "configFile_32.txt", "configFile_46.txt", "configFile_58.txt", "configFile_13.txt", "configFile_33.txt", "configFile_51.txt", "configFile_59.txt", "configFile_14.txt", "configFile_34.txt", "configFile_52.txt", "configFile_21.txt", "configFile_41.txt", "configFile_53.txt", "configFile_22.txt", "configFile_42.txt", "configFile_54.txt"}
 
         repeat with cfgFile in configFiles
             set newTab to (create tab with default profile)
