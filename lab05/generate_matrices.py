a69da26319ef5bacac2fd13db785f6063d3bb4df
Sayan Goswami <goswami.sayan47@gmail.com>
2021-06-05 16:15:03 +0200

    Add script to generate matrices.

---
+++ lab05/generate_matrices.py
@@ -0,0 +1,36 @@
+import argparse
+import random
+
+def main(dim_a, dim_b):
+    print("Writing matrix A to a.mat")
+    with open("a.mat", "wt") as matrix_a_file:
+        for row_a in range(dim_a[0]):
+            for col_a in range(dim_a[1]):
+                matrix_a_file.write(f"{row_a},{col_a},{random.random() * 100:.2f}\n")
+
+    print("Writing matrix B to b.mat")
+    with open("b.mat", "wt") as matrix_b_file:
+        for row_b in range(dim_b[0]):
+            for col_b in range(dim_b[1]):
+                matrix_b_file.write(f"{row_b},{col_b},{random.random() * 100:.2f}\n")
+
+    print("Done.")
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--ra", default=0, type=int, required=True)
+    parser.add_argument("--ca", default=0, type=int, required=True)
+    parser.add_argument("--rb", default=0, type=int, required=True)
+    parser.add_argument("--cb", default=0, type=int, required=True)
+
+    args = parser.parse_args()
+
+    dim_a =(args.ra, args.ca)
+    dim_b =(args.rb, args.cb)
+
+    assert args.ca == args.rb, "m x n matrix can only be multiplied with a n x o matrix."
+
+    main(dim_a, dim_b)
